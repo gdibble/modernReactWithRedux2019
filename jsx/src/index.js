f2fda9db6a821c2beebcd2ee066ac4b0cbaea054
Gabriel Dibble <gdibble@gmail.com>
2019-09-04 23:17:22 -0700

    Section 3: JSX labelText const + usage

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -5,10 +5,13 @@ import ReactDOM from 'react-dom'
 // Create a React Component
 const App = () => {
   const buttonText = { text: 'Click me' };
+  const labelText = 'Enter name: ';
 
   return (
     <div>
-      <label className="label" for="name">Enter name: </label>
+      <label className="label" for="name">
+        {labelText}
+      </label>
       <input id="name" type="text" class="label" />
       <button style={{ backgroundColor:'teal', color:'white' }}>
         {buttonText.text}
