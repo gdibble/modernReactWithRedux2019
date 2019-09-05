0f59651d080108d466ad2fca964d2538220b2299
Gabriel Dibble <gdibble@gmail.com>
2019-09-04 23:04:30 -0700

    Section 2: JSX getButtonText fn + usage

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,15 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+function getButtonText() {
+  return 'Click on me!';
+}
+
 // Create a React Component
 const App = () => {
-  const buttonText = 'Click Me';
-
   return (
     <div>
       <label className="label" for="name">Enter name: </label>
       <input id="name" type="text" class="label" />
-      <button style={{ backgroundColor:'teal', color:'white' }}>{buttonText}</button>
+      <button style={{ backgroundColor:'teal', color:'white' }}>
+        {getButtonText()}
+      </button>
     </div>
   );
 };
