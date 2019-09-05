1b9fc0ffd7f2889b4ea184db9d61d54a10e3ed74
Gabriel Dibble <gdibble@gmail.com>
2019-09-04 22:01:00 -0700

    Section 1: JSX app primer

---
+++ jsx/src/index.js
@@ -0,0 +1,11 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+// Create a React Component
+const App = () => {
+  return <div>Hi there!</div>;
+};
+
+// Show React Component in browser
+ReactDOM.render(<App />, document.getElementById('root'))
