0a877546735991d9672e78aaa44bc6a210378ff0
Gabriel Dibble <gdibble@gmail.com>
2019-09-05 00:41:33 -0700

    Section 3: CommentDetail author as Faker.name.firstName()

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,9 @@ import Faker from 'faker'
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45 AM" text="Comment 1" avatarSrc={Faker.image.avatar()} />
-      <CommentDetail author="Alex" timeAgo="Today at 2:00 AM" text=" Comment 2" avatarSrc={Faker.image.avatar()} />
-      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00 PM" text="Comment 3" avatarSrc={Faker.image.avatar()} />
+      <CommentDetail author={Faker.name.firstName()} timeAgo="Today at 4:45 AM" text="Comment 1" avatarSrc={Faker.image.avatar()} />
+      <CommentDetail author={Faker.name.firstName()} timeAgo="Today at 2:00 AM" text=" Comment 2" avatarSrc={Faker.image.avatar()} />
+      <CommentDetail author={Faker.name.firstName()} timeAgo="Yesterday at 5:00 PM" text="Comment 3" avatarSrc={Faker.image.avatar()} />
     </div>
   );
 };
