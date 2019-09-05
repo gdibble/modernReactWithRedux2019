20ee86737d276285213a34156d31f66857f8d1fc
Gabriel Dibble <gdibble@gmail.com>
2019-09-05 00:04:46 -0700

    Section 3: 3X Comments

--- components/src/index.js
+++ components/src/index.js
@@ -23,6 +23,42 @@ const App = () => {
           Nice blog post!
         </div>
       </div>
+      <div className="comment">
+        <a className="avatar" href="/">
+          <img src={Faker.image.avatar()} alt="Avatar" />
+        </a>
+        <div className="content">
+          <a className="author" href="/">
+            Sam
+          </a>
+        </div>
+        <div className="metadata">
+          <span className="date">
+            Today at 6:00 PM
+          </span>
+        </div>
+        <div className="text">
+          Nice blog post!
+        </div>
+      </div>
+      <div className="comment">
+        <a className="avatar" href="/">
+          <img src={Faker.image.avatar()} alt="Avatar" />
+        </a>
+        <div className="content">
+          <a className="author" href="/">
+            Sam
+          </a>
+        </div>
+        <div className="metadata">
+          <span className="date">
+            Today at 6:00 PM
+          </span>
+        </div>
+        <div className="text">
+          Nice blog post!
+        </div>
+      </div>
     </div>
   );
 };
