4f4a9522a0a3217e0ce85f96560a0c7701d0674f
Gabriel Dibble <gdibble@gmail.com>
2019-09-05 00:20:41 -0700

    Section 3: 5X CommentDetail components

--- components/src/index.js
+++ components/src/index.js
@@ -6,6 +6,10 @@ const App = () => {
   return (
     <div className="ui container comments">
       <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   );
 };
