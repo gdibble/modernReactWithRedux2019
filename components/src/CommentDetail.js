f9e4c703b435a4b8ce278726396d61cde7be8030
Gabriel Dibble <gdibble@gmail.com>
2019-09-05 00:10:26 -0700

    Section 3: Basic CommentDetail component + export

---
+++ components/src/CommentDetail.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Faker from 'faker'
+
+const CommentDetail = () => {
+  return (
+      <div className="comment">
+      <a className="avatar" href="/">
+        <img src={Faker.image.avatar()} alt="Avatar" />
+      </a>
+      <div className="content">
+        <a className="author" href="/">
+          Sam
+        </a>
+      </div>
+      <div className="metadata">
+        <span className="date">
+          Today at 6:00 PM
+        </span>
+      </div>
+      <div className="text">
+        Nice blog post!
+      </div>
+    </div>
+  );
+};
+
+export default CommendDetail;
