5c88abecce9447711707d25457e01a3f7bbc6e59
Gabriel Dibble <gdibble@gmail.com>
2019-09-05 23:03:42 -0700

    Section 4: Log geolocation API position

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),  // Success Callback
+    (error) => console.log(error)         // Error Callback
+  );
+
   return (
     <SeasonDisplay />
   );
