f4ddd7b2c17d7aadd4b95792bd576e61dccd7109
Gabriel Dibble <gdibble@gmail.com>
2019-09-06 00:32:32 -0700

    Section 5: Basic location error handling

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,11 +6,14 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);  // Parent setup functionality
-    this.state = { lat:null };
+    this.state = {
+      lat: null,
+      errorMessage: ''
+    };
     // Immediate request location:
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat:position.coords.latitude }),  // Success Callback
-      (error) => console.log(error)                                   // Error Callback
+      (error) => this.setState({ errorMessage:error.message })        // Error Callback
     );
   }
 
@@ -18,6 +21,8 @@ class App extends React.Component {
     return (
       <div>
         Latitude: { this.state.lat }
+        <br />
+        Error: { this.state.errorMessage }
       </div>
     );
   }
