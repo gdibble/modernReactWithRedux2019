edd6c6244d4b533c3207a67ae9912c8c9cdcdb9b
Gabriel Dibble <gdibble@gmail.com>
2019-09-05 23:00:11 -0700

    Section 4: SeasonDisplay import & basic usage

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 const App = () => {
   return (
-    <div>Hi there!</div>
+    <SeasonDisplay />
   );
 };
 
