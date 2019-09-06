acc309ab3c55d79fd8f05566ffc7683f744b236e
Gabriel Dibble <gdibble@gmail.com>
2019-09-05 23:35:15 -0700

    Section 4: Keep class; cleanup old fn component code

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,19 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-/*
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),  // Success Callback
-    (error) => console.log(error)         // Error Callback
-  );
-
-  return (
-    <SeasonDisplay />
-  );
-};
-*/
-
 class App extends React.Component {
 
   render() {
