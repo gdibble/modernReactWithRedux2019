ddc97a8f7dc238d3a2eae1d660c6cc9163133e37
Gabriel Dibble <gdibble@gmail.com>
2019-09-11 01:00:26 -0700

    Section 7: App in components directory + use

--- pics/src/index.js
+++ pics/src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-  return (
-    <div></div>
-  );
-};
+import App from './components/App.js';
 
 ReactDOM.render(<App />, document.getElementById('root'))
