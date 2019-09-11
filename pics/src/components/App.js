87d3d961f61195af53b046727eb0f708c9d5259a
Gabriel Dibble <gdibble@gmail.com>
2019-09-11 01:07:33 -0700

    Section 7: Basic SearchBar component + use

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import SearchBar from './SearchBar.js'
 
 class App extends React.Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 };
