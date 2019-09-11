41684f677f220321b4d43b4684eb4471b8f412d3
Gabriel Dibble <gdibble@gmail.com>
2019-09-11 02:35:14 -0700

    Section 7: SearchBar onSubmit Log arrow fn example (fixed)

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
-  onFormSubmit(event) {
+  onFormSubmit = (event) => {
     event.preventDefault();
 
     console.log(this.state.term);
