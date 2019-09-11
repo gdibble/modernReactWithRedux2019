371645dbfac6e8f1a67428aeb1fcbf238cdfd4b8
Gabriel Dibble <gdibble@gmail.com>
2019-09-11 01:19:47 -0700

    Section 7: Basic App container styling

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,7 +4,7 @@ import SearchBar from './SearchBar.js'
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar />
       </div>
     );
