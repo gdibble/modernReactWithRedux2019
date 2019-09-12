06ff9b101633a41d911ae942be1339933edc7774
Gabriel Dibble <gdibble@gmail.com>
2019-09-12 00:19:54 -0700

    Section 10: Basic ImageCard imageRef setup/use

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
+
   render() {
     const { urls, description } = this.props.image;
     return (
       <div>
         <img
+          ref={this.imageRef}
           src={urls.regular}
           alt={description}
           title={description}
