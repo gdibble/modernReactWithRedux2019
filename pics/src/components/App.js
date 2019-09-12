404ebe9898841acf2f291b9a4deff06d23bad0ce
Gabriel Dibble <gdibble@gmail.com>
2019-09-11 22:07:21 -0700

    Revert "Section 8: Log response of request [chained then] promise"
    This reverts commit a1a8992a225fdb64d2c67339e20c7a1233e5a172.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -7,10 +7,8 @@ class App extends React.Component {
     axios.get('https://api.unsplash.com/search/photos', {
       headers: { 'Authorization': 'Client-ID 1a3018b68eae408c7f49377edd3543843e3ea56875f8d1189a6baf17cf48386e' },
       params: { query: term }
-    }).then(response => {
-      console.log(response.data.results);
-    });
-  }
+    })
+  };
 
   render() {
     return (
