75d96f010ea29f533e24f844ae568a77aaba226e
Gabriel Dibble <gdibble@gmail.com>
2019-09-12 21:12:53 -0700

    Section 11: Boilerplate video app

---
+++ videos/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+  state = {
+    // ...
+  };
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
