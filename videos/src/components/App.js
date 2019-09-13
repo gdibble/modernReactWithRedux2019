8a5ba73b5f6f544141a78e7cfcc4644358d5e7d5
Gabriel Dibble <gdibble@gmail.com>
2019-09-12 22:32:36 -0700

    Section 11: Set videos response to state + length output

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -5,7 +5,7 @@ import YouTube from '../apis/YouTube.js';
 class App extends React.Component {
 
   state = {
-    // ...
+    videos: []
   };
 
   onTermSubmit = async term => {
@@ -13,13 +13,14 @@ class App extends React.Component {
       params: { q: term }
     });
 
-    console.log(response);
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length} vidoes.
       </div>
     );
   }
