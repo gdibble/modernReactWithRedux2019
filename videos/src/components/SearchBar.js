3aaadaf63789b0ea0ac5951545b9b5f32df867e4
Gabriel Dibble <gdibble@gmail.com>
2019-09-12 21:48:47 -0700

    Section 11: SearchBar form onSubmit handler

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -6,14 +6,23 @@ class SearchBar extends React.Component {
     term: ''
   };
 
-  onInputChange = (event) => {
+  onInputChange = event => {
     this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    // Call parent component callback...
   };
 
   render() {
     return (
       <div className="ui segment search-bar">
-        <form className="ui form">
+        <form
+          className="ui form"
+          onSubmit={this.onFormSubmit}
+        >
           <div className="field">
             <label htmlFor="search">
               Video Search
