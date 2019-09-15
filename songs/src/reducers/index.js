3149d605d48d2ff08c7735d240bc7f27757ba450
Gabriel Dibble <gdibble@gmail.com>
2019-09-14 23:28:52 -0700

    Section 13: Export combined reducers

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 /**
  * @method songsReducer
  * @summary Action creator
@@ -29,3 +31,8 @@ export const selectedSongReducer = (selectedSong = null, action) => {
 
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
