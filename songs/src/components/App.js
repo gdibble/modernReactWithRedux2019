36b6461334bfdf6969e5ac8f0c42b737888d4b6f
Gabriel Dibble <gdibble@gmail.com>
2019-09-14 23:44:32 -0700

    Section 13: Basic SongList component + use

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SongList from './SongList.js';
 import { selectSong } from '../actions';
 
 const App = () => {
   return (
     <div>
-      App
+      <SongList />
     </div>
   );
 };
---
+++ songs/src/components/SongList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SongList extends Component {
+
+  render() {
+    return (
+      <div>
+        Song List
+      </div>
+    );
+  }
+
+}
+
+export default SongList;
