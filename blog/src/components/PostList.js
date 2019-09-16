8022b745426ddb44dee8d24dff5ad83299e1d12d
Gabriel Dibble <gdibble@gmail.com>
2019-09-15 22:07:19 -0700

    Section 14: PostList using fetchPosts action

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends Component {
+
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
 
   render() {
     return (
@@ -12,4 +18,4 @@ class PostList extends Component {
 
 }
 
-export default PostList;
+export default connect(null, { fetchPosts })(PostList);
