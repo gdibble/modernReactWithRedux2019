ea502173faafa8b8c4d5824d65ae358db2d4c68f
Gabriel Dibble <gdibble@gmail.com>
2019-09-15 21:35:45 -0700

    Section 14: Boilerplate blog app

---
+++ blog/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { CreateStore, createStore } from 'redux';
+
+// App Code
+import App from './components/App.js';
+import reducers from './reducers';
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
