ce45813efa546b533391ab7dda073e6a439fb60a
Gabriel Dibble <gdibble@gmail.com>
2019-09-15 21:52:29 -0700

    Section 14: Basic PostList component + use

--- blog/src/components/App.js
+++ blog/src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PostList from './PostList.js';
 
 const App = () => {
   return (
     <div className="ui container">
-      App
+      <PostList />
     </div>
   );
 };
