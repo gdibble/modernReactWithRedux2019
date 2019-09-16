482344af2c407a972a55a0ed4d8875fc874b1ebc
Gabriel Dibble <gdibble@gmail.com>
2019-09-15 22:18:26 -0700

    Section 14: Actions with bad approach to get data (error!)

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,11 +1,17 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder.js';
+
 /**
  * @method fetchPosts
  * @summary Action creator
  *
  * @returns {Object} - action
 */
-export const fetchPosts = song => {
+export const fetchPosts = async () => {
+  // Bad approach!
+  const response = await jsonPlaceholder.get('/posts');
+
   return {
-    type: 'FETCH_POSTS'
+    type: 'FETCH_POSTS',
+    payload: response
   };
 };
