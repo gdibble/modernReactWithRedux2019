1de14c7e71d5abd5c89a45538371fb43f8669e03
Gabriel Dibble <gdibble@gmail.com>
2019-09-16 21:20:41 -0700

    Section 15: Examples of what NOT to do in a reducer

--- blog/src/reducers/postsReducer.js
+++ blog/src/reducers/postsReducer.js
@@ -1,3 +1,8 @@
-export default () => {
-  return 123;
+export default (state, action) => {
+  // Never mutate state:
+  state[0] = 'Sam';
+  state.pop();
+  state.push();
+  state.name = 'Same';
+  state.age = '30';
 };
