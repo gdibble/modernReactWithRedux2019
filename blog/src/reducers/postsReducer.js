af31fcc26ebadcbc317b63abb77f806cddeb91a5
Gabriel Dibble <gdibble@gmail.com>
2019-09-16 21:22:57 -0700

    Revert "Section 15: Examples of what NOT to do in a reducer"
    This reverts commit 1de14c7e71d5abd5c89a45538371fb43f8669e03.

--- blog/src/reducers/postsReducer.js
+++ blog/src/reducers/postsReducer.js
@@ -1,8 +1,3 @@
-export default (state, action) => {
-  // Never mutate state:
-  state[0] = 'Sam';
-  state.pop();
-  state.push();
-  state.name = 'Same';
-  state.age = '30';
+export default () => {
+  return 123;
 };
