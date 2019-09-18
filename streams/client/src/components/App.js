740bb12cf134986cbc945d706963e29cdc30b8f3
Gabriel Dibble <gdibble@gmail.com>
2019-09-17 21:24:20 -0700

    Section 16: Basic react-router-dom example

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+const PageOne = () => {
+  return <div>Page One</div>;
+};
+
+const PageTwo = () => {
+  return <div>Page Two <button>Click me!</button></div>;
+};
+
 const App = () => {
   return (
     <div className="ui container">
-      App
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/2" component={PageTwo} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
