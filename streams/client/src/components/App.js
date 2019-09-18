3d57097e5012521447f541c7225af1b02aa71d09
Gabriel Dibble <gdibble@gmail.com>
2019-09-17 21:45:30 -0700

    Section 16: Traditional navigation with hyperlinks

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 const PageOne = () => {
-  return <div>Page One</div>;
+  return (
+    <div>
+      Page One
+      <a href="/2">Navigate to pg.2</a> BAD!
+    </div>
+  );
 };
 
 const PageTwo = () => {
-  return <div>Page Two <button>Click me!</button></div>;
+  return (
+    <div>
+      Page Two
+      <button>Click me!</button>
+      <a href="/">Navigate to pg.1</a> BAD!!
+    </div>
+  );
 };
 
 const App = () => {
