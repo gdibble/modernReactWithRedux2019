6428e4121f8d89d5c81a6a461d3431af70bf1ed2
Gabriel Dibble <gdibble@gmail.com>
2019-09-18 20:50:32 -0700

    Section 17: Basic GoogleAuth component + use in Header

---
+++ streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+
+  render() {
+    return (
+      <div className="">
+        Google Auth
+      </div>
+    );
+  }
+
+}
+
+export default GoogleAuth;
