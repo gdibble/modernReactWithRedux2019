929e6cc3eb7ee599d162ad8f8d05b626f50743b0
Gabriel Dibble <gdibble@gmail.com>
2019-09-18 20:55:36 -0700

    Section 17: GoogleAuth component loading client:auth2

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 class GoogleAuth extends React.Component {
 
+  componentDidMount() {
+    window.gapi.load('client:auth2');
+  }
+
   render() {
     return (
       <div className="">
