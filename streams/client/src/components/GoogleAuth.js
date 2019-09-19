e44ebb8d70c5eb8abf28c43d6452b5e8033e1186
Gabriel Dibble <gdibble@gmail.com>
2019-09-18 21:37:59 -0700

    Section 17: GoogleAuth sign in/out button click events

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -21,20 +21,34 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null)
       return null;
 
     if (this.state.isSignedIn)
       return (
-        <button className="ui red google button">
+        <button
+          className="ui red google button"
+          onClick={this.onSignOut}
+        >
           <i className="google icon" />
           Sign Out
         </button>
       );
 
     return (
-      <button className="ui red google button">
+      <button
+        className="ui red google button"
+        onClick={this.onSignIn}
+      >
         <i className="google icon" />
         Sign In with Google
       </button>
