9adf64b35753364606223ecc66aeda8708b0e849
Gabriel Dibble <gdibble@gmail.com>
2019-09-18 20:58:46 -0700

    Section 17: GoogleAuth component initiates OAuth2 lib

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -3,7 +3,12 @@ import React from 'react';
 class GoogleAuth extends React.Component {
 
   componentDidMount() {
-    window.gapi.load('client:auth2');
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: '459147754039-ai2vbl6ekblbd8ee2icdb04e93a0b6tj.apps.googleusercontent.com',
+        scope: 'email'
+      });
+    });
   }
 
   render() {
