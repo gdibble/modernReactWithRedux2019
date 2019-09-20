9db98d72ccf9bb2c7a600341bf4f3755ead2fb59
Gabriel Dibble <gdibble@gmail.com>
2019-09-19 23:01:37 -0700

    Section 19: Input autoComplete=off

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -7,7 +7,7 @@ class StreamCreate extends React.Component {
     return (
       <div className="field">
         <label>{label}</label>
-        <input { ...input } />
+        <input { ...input } autoComplete="off" />
         <div>{meta.error}</div>
       </div>
     );
