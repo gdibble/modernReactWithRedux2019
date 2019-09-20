1252471d4140dded6366cea0323226ee39558e58
Gabriel Dibble <gdibble@gmail.com>
2019-09-19 21:03:42 -0700

    Section 19: Rename reducer to formReducer

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
-import { reducer } from 'redux-form';
+import { reducer as formReducer } from 'redux-form';
 
 import authReducer from './authReducer.js';
 
 export default combineReducers({
   auth: authReducer,
-  form: reducer
+  form: formReducer
 });
