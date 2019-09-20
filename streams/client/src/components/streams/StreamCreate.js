4967aeb3030c19310af891a6baff56df067b3f44
Gabriel Dibble <gdibble@gmail.com>
2019-09-19 23:14:11 -0700

    Section 19: Conditional className on input via renderInput

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -15,8 +15,9 @@ class StreamCreate extends React.Component {
   }
 
   renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`
     return (
-      <div className="field error">
+      <div className={className}>
         <label>{label}</label>
         <input { ...input } autoComplete="off" />
         {this.renderError(meta)}
