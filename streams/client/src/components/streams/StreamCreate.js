a322499fd9ecf64986af75ba0b5bee5cfdf60506
Gabriel Dibble <gdibble@gmail.com>
2019-09-19 22:25:28 -0700

    Section 19: Field with custom label param + use

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -3,15 +3,20 @@ import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
 
-  renderInput({ input }) {
-    return <input { ...input } />;
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input { ...input } />
+      </div>
+    );
   }
 
   render() {
     return (
       <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field name="description" component={this.renderInput} label="Enter Description" />
       </form>
     );
   }
