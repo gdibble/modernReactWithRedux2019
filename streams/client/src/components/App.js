27a9b383d3fba4b0373fba89e5985b728ef5a2f6
Gabriel Dibble <gdibble@gmail.com>
2019-09-19 21:32:39 -0700

    Section 19: Routes should use exact param

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -14,10 +14,10 @@ const App = () => {
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit" component={StreamEdit} />
-          <Route path="/streams/delete" component={StreamDelete} />
-          <Route path="/streams/show" component={StreamShow} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
