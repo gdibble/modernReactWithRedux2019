4bce4d1b33f328b7a296e79e96ceda790767e05c
Gabriel Dibble <gdibble@gmail.com>
2019-09-22 23:11:16 -0700

    Section 20: StreamList as class based component + componentDidMount

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams }  from '../../actions';
 
-const StreamList = () => {
-  return (
-    <div className="">
-      Stream List
-    </div>
-  );
+class StreamList extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  render() {
+    return (
+      <div className="">
+        Stream List
+      </div>
+    );
+  }
+
 };
 
 export default connect(null, { fetchStreams })(StreamList);
