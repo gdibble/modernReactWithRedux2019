e7d25408a9dfd9f7de84f720b4d9d2e49e628791
Gabriel Dibble <gdibble@gmail.com>
2019-09-22 23:06:28 -0700

    Section 20: Import streamReducer in combineReducers

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
 import authReducer from './authReducer.js';
+import streamReducer from './streamReducer.js';
 
 export default combineReducers({
   auth: authReducer,
-  form: formReducer
+  form: formReducer,
+  streams: streamReducer
 });
