7aeb1bbe0d4077e48674e833e7e58e8e7db9a48e
Gabriel Dibble <gdibble@gmail.com>
2019-09-23 00:15:11 -0700

    Section 20: StreamList using renderList and basic output

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -8,11 +8,32 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div
+          key={stream.id}
+          className="item"
+        >
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">
+              {stream.description}
+            </div>
+          </div>
+        </div>
+      );
+    })
+  };
+
   render() {
-    console.log(this.props.streams);
     return (
-      <div className="">
-        Stream List
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">
+          {this.renderList()}
+        </div>
       </div>
     );
   }
