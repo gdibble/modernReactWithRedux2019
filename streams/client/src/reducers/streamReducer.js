2e39428ca5a3b7930f3b842a6149a2e9c7b2d900
Gabriel Dibble <gdibble@gmail.com>
2019-09-22 22:53:09 -0700

    Section 20: streaReducer w/ create/edit/fetchStream

---
+++ streams/client/src/reducers/streamReducer.js
@@ -0,0 +1,18 @@
+import {
+  CREATE_STREAM,
+  EDIT_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM
+} from '../actions/types.js';
+import streams from '../apis/streams.js';
+
+export default (state = {}, action) => {
+  switch(action.type) {
+    case CREATE_STREAM:
+    case EDIT_STREAM:
+    case FETCH_STREAM:
+        return { ...state, [action.payload.id]: action.payload };
+    default:
+      return state;
+  }
+};
