758f838fdb12735042723b1a0940ce6663b43996
Gabriel Dibble <gdibble@gmail.com>
2019-09-23 21:49:24 -0700

    Section 20: StreamList renderCreate + Link to create pg

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams }  from '../../actions';
 
 class StreamList extends React.Component {
@@ -38,6 +39,20 @@ class StreamList extends React.Component {
     })
   };
 
+  renderCreate() {
+    if (this.props.isSignedIn)
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link
+            to="/streams/new"
+            className="ui button primary"
+          >
+            Create Stream
+          </Link>
+        </div>
+      );
+  }
+
   render() {
     return (
       <div>
@@ -45,6 +60,7 @@ class StreamList extends React.Component {
         <div className="ui celled list">
           {this.renderList()}
         </div>
+        {this.renderCreate()}
       </div>
     );
   }
@@ -54,7 +70,8 @@ class StreamList extends React.Component {
 const mapStateToProps = state => {
   return {
     streams: Object.values(state.streams),
-    currentUserId: state.auth.userId
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
   };
 };
 
