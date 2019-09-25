84697b4ebce689ef0d6c298aabc6caf99638882f
Gabriel Dibble <gdibble@gmail.com>
2019-09-24 22:58:40 -0700

    Section 21: Modal UI updates

--- streams/client/src/components/Modal.js
+++ streams/client/src/components/Modal.js
@@ -5,7 +5,16 @@ const Modal = () => {
   return ReactDOM.createPortal(
     <div className="ui dimmer modals visible active">
       <div className="ui standard modal visible active">
-        Modal
+        <div className="header">
+          Delete Stream
+        </div>
+        <div className="content">
+          Are you sure that you want to delete this stream?
+        </div>
+        <div className="actions">
+          <button className="ui button primary">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
       </div>
     </div>,
     document.getElementById('modal')
