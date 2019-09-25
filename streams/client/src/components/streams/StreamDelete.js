56d576948bc0fe19c8d895a9800e789c2dd1e089
Gabriel Dibble <gdibble@gmail.com>
2019-09-24 23:21:29 -0700

    Section 21: Modal actions React.Fragment shorthand <></>

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -3,10 +3,10 @@ import Modal from '../Modal.js';
 
 const StreamDelete = () => {
   const actions = (
-    <React.Fragment>
+    <>
       <button className="ui button negative">Delete</button>
       <button className="ui button cancel">Cancel</button>
-    </React.Fragment>
+    </>
   );
 
   return (
