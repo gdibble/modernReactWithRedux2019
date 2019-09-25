988205c29dacaea97efab1dce63c71b1a74717ba
Gabriel Dibble <gdibble@gmail.com>
2019-09-24 22:52:57 -0700

    Section 21: Modal basic integration in StreamDelete

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Modal from '../Modal.js';
 
 const StreamDelete = () => {
   return (
     <div className="">
       Stream Delete
+      <Modal />
     </div>
   );
 };
