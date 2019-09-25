21011a2187fd81460dda15e99a25199c76f5bff6
Gabriel Dibble <gdibble@gmail.com>
2019-09-24 23:38:26 -0700

    Section 21: StreamDelete as class + renderActions helper

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -2,25 +2,31 @@ import React from 'react';
 import Modal from '../Modal.js';
 import history from '../../history.js';
 
-const StreamDelete = () => {
-  const actions = (
-    <React.Fragment>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button cancel">Cancel</button>
-    </React.Fragment>
-  );
+class StreamDelete extends React.Component {
 
-  return (
-    <div className="">
-      Stream Delete
-      <Modal
-        title="Delete Stream"
-        content="Are you sure that you want to delete this stream?"
-        actions={actions}
-        onDismiss={() => history.push('/')}
-      />
-    </div>
-  );
-};
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button cancel">Cancel</button>
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <div className="">
+        Stream Delete
+        <Modal
+          title="Delete Stream"
+          content="Are you sure that you want to delete this stream?"
+          actions={this.renderActions()}
+          onDismiss={() => history.push('/')}
+        />
+      </div>
+    );
+  }
+
+}
 
 export default StreamDelete;
