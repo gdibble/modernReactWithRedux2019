02ee77e281b2d0f629636cbe0b70aeb60a1999da
Gabriel Dibble <gdibble@gmail.com>
2019-09-26 00:03:50 -0700

    Section 23: Button text const using context + output

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -6,9 +6,11 @@ class Button extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    console.log(this.context);
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
     return (
-      <button className="ui button primary">Submit</button>
+      <button className="ui button primary">
+        {text}
+      </button>
     );
   }
 
