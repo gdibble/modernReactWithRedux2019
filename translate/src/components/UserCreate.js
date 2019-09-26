0d2308b8e5e1b8036dbac963c04015622c5e030e
Gabriel Dibble <gdibble@gmail.com>
2019-09-25 23:33:37 -0700

    Section 23: Basic UserCreate component

---
+++ translate/src/components/UserCreate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Field from './Field.js';
+import Button from './Button.js';
+
+const UserCreate = () => {
+  return (
+    <div>
+      <Field />
+      <Button />
+    </div>
+  );
+};
+
+export default UserCreate;
