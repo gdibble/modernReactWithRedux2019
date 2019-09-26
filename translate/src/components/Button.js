0d57647cc4e7bb08480cb0a316f8eae6a91aed81
Gabriel Dibble <gdibble@gmail.com>
2019-09-25 23:52:42 -0700

    Section 23: Button import LanguageContext and wireup with contextType

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import LanguageContext from '../context/LanguageContext.js';
 
 class Button extends React.Component {
+
+  static contextType = LanguageContext;
 
   render() {
     return (
