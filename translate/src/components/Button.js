57fdf7fe38884bad85e076ccefa11e3e8d50a1e2
Gabriel Dibble <gdibble@gmail.com>
2019-09-26 00:29:12 -0700

    Section 23: Button + renderSubmit helper

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -3,11 +3,15 @@ import LanguageContext from '../context/LanguageContext.js';
 
 class Button extends React.Component {
 
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen';
+  }
+
   render() {
     return (
       <button className="ui button primary">
         <LanguageContext.Consumer>
-          {value => value === 'english' ? 'Submit' : 'Voorleggen' }
+          {value => this.renderSubmit(value) }
         </LanguageContext.Consumer>
       </button>
     );
