7340018f7d74c98a53b0ec0f785c0e27aff7f9d8
Gabriel Dibble <gdibble@gmail.com>
2019-09-25 23:29:14 -0700

    Section 23: App flag onClicks calling onLanguageChange

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -4,14 +4,19 @@ class App extends React.Component {
 
   state = { language: 'english' };
 
+  onLanguageChange = language => {
+    this.setState({ language });
+  }
+
   render() {
     return (
       <div className="ui container">
         <div>
           Select a language:
-          <i className="flag us" />
-          <i className="flag nl" />
+          <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
         </div>
+        {this.state.language}
       </div>
     );
   }
