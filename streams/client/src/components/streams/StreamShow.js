e93dbe30abcc999681676fb331cf197773db2071
Gabriel Dibble <gdibble@gmail.com>
2019-09-25 20:57:02 -0700

    Section 22: StreamShow as class based component

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStream }  from '../../actions';
 
-const StreamShow = () => {
-  return (
-    <div className="">
-      Stream Show
-    </div>
-  );
+class StreamShow extends React.Component {
+
+  render() {
+    return (
+      <div className="">
+        Stream Show
+      </div>
+    );
+  }
+
 };
 
 export default connect(null, { fetchStream })(StreamShow);
