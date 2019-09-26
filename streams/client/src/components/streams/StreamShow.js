6ea2ace1832b8e35b4c815dcdfb5d4d48086708f
Gabriel Dibble <gdibble@gmail.com>
2019-09-25 20:58:00 -0700

    Section 22: StreamShow componentDidMount + fetchStream called

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -4,6 +4,10 @@ import { fetchStream }  from '../../actions';
 
 class StreamShow extends React.Component {
 
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
   render() {
     return (
       <div className="">
