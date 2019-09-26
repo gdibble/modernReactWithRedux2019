ae559af3614ce4d709c17f2a6e587ef215a1eee0
Gabriel Dibble <gdibble@gmail.com>
2019-09-26 00:22:46 -0700

    Revert "Section 23: App example of UserCreate w/ Provider defaultValue"
    This reverts commit e57ca7414b8981fdf4f6d2de286bdbd08d4c2420.

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -21,9 +21,6 @@ class App extends React.Component {
         <LanguageContext.Provider value={this.state.language}>
           <UserCreate />
         </LanguageContext.Provider>
-        <LanguageContext.Provider value="dutch">
-          <UserCreate />
-        </LanguageContext.Provider>
       </div>
     );
   }
