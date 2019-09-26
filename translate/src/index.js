d4d8de890c2a125dd28015e28baa2aed31602cce
Gabriel Dibble <gdibble@gmail.com>
2019-09-25 23:03:46 -0700

    Section 23: Boilerplate translate app

---
+++ translate/src/index.js
@@ -0,0 +1,9 @@
+// Dependencies
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
