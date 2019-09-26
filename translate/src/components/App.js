e10e9f6240fc535e9a8e58757465845f1ef9aa55
Gabriel Dibble <gdibble@gmail.com>
2019-09-25 23:26:39 -0700

    Section 23: App with text and two flag icons

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 class App extends React.Component {
 
+  state = { language: 'english' };
+
   render() {
     return (
       <div className="ui container">
-        App
+        <div>
+          Select a language:
+          <i className="flag us" />
+          <i className="flag nl" />
+        </div>
       </div>
     );
   }
