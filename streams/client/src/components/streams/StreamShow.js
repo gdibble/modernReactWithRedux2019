fc6a31c8bc0e897580311c006d82e4b1c79716da
Gabriel Dibble <gdibble@gmail.com>
2019-09-25 21:01:41 -0700

    Section 22: StreamShow mapStateToProps + show title/desc

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -9,13 +9,25 @@ class StreamShow extends React.Component {
   }
 
   render() {
+    if (!this.props.stream)
+      return <div>Loading...</div>;
+
     return (
       <div className="">
-        Stream Show
+        <h1>
+          {this.props.stream.title}
+        </h1>
+        <h5>
+          {this.props.stream.description}
+        </h5>
       </div>
     );
   }
 
 };
 
-export default connect(null, { fetchStream })(StreamShow);
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
