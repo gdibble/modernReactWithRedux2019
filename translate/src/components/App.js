e57ca7414b8981fdf4f6d2de286bdbd08d4c2420
Gabriel Dibble <gdibble@gmail.com>
2019-09-26 00:21:19 -0700

    Section 23: App example of UserCreate w/ Provider defaultValue

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -21,6 +21,9 @@ class App extends React.Component {
         <LanguageContext.Provider value={this.state.language}>
           <UserCreate />
         </LanguageContext.Provider>
+        <LanguageContext.Provider value="dutch">
+          <UserCreate />
+        </LanguageContext.Provider>
       </div>
     );
   }
