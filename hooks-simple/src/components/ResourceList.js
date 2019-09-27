135fed6c7b11477b9bc9b750eb7bceef25840b9b
Gabriel Dibble <gdibble@gmail.com>
2019-09-27 00:13:43 -0700

    Section 25: useResources component + use in ResourceList

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,16 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-const useResources = resource => {
-  const [ resources, setResources ] = useState([]);
-  useEffect(() => {
-    (async resource => {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
-      setResources(response.data);
-    })(resource);
-  }, [ resource ]);
-  return resources;
-};
+import React from 'react';
+import useResources from './useResources.js';
 
 const ResourceList = ({ resource }) => {
   const resources = useResources(resource);
