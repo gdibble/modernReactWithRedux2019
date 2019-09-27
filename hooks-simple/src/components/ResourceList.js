b34efd1389d2f93cda3068502d10dbca9152c440
Gabriel Dibble <gdibble@gmail.com>
2019-09-26 23:25:21 -0700

    Section 25: ResourceList + axios fetching posts

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import axios from 'axios';
 
 class ResourceList extends React.Component {
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+  }
 
   render() {
     return (
