e3ca722d5f3a4f53d8f23793a08a05f2e1c8c346
Gabriel Dibble <gdibble@gmail.com>
2019-09-26 22:28:28 -0700

    Section 24: App + LanguageStore component

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import UserCreate from './UserCreate.js';
 import ColorContext from '../context/ColorContext.js';
-import LanguageContext from '../context/LanguageContext.js';
+import { LanguageStore } from '../context/LanguageContext.js';
 import LanguageSelector from './LanguageSelector.js';
 
 class App extends React.Component {
 
-  state = { language: 'english' };
-
-  onLanguageChange = language => {
-    this.setState({ language });
-  }
-
   render() {
     return (
       <div className="ui container">
-        <LanguageSelector onLanguageChange={this.onLanguageChange} />
-        <ColorContext.Provider value="primary">
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value="primary">
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
