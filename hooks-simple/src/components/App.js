c5800159ca76005b3bd55986e46b831c8f7c3679
Gabriel Dibble <gdibble@gmail.com>
2019-09-26 23:08:45 -0700

    Section 25: App refactor using useState

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,21 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class App extends React.Component {
-
-  state = { resource: 'posts' };
-
-  render() {
-    return (
-      <div className="ui container">
-        <div>
-          <button onClick={() => this.setState({ resource: 'posts' })}>Posts</button>
-          <button onClick={() => this.setState({ resource: 'todos' })}>ToDos</button>
-        </div>
-        {this.state.resource}
+const App = () => {
+  const [ resource, setResourece ] = useState('posts');
+  return (
+    <div className="ui container">
+      <div>
+        <button onClick={() => setResourece('posts')}>Posts</button>
+        <button onClick={() => setResourece('todos')}>ToDos</button>
       </div>
-    );
-  }
-
+      {resource}
+    </div>
+  );
 };
 
 export default App;
