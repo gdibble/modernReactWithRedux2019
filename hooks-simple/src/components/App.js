e2114a10bd992a4aad629049da6ce4a25b1b1b9f
Gabriel Dibble <gdibble@gmail.com>
2019-09-27 00:18:04 -0700

    Section 25: UserList component reusing useResources

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import ResoureceList from './ResourceList.js';
+import UserList from './UserList.js';
 
 const App = () => {
   const [ resource, setResourece ] = useState('posts');
   return (
     <div className="ui container">
+      <UserList />
       <div>
         <button onClick={() => setResourece('posts')}>Posts</button>
         <button onClick={() => setResourece('todos')}>ToDos</button>
