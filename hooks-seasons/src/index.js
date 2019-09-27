8f905ea5a07407ee6c9002b0dfa0659b2c15a7cd
Gabriel Dibble <gdibble@gmail.com>
2019-09-27 00:39:56 -0700

    Section 26: Refactor index.js to functional component

--- hooks-seasons/src/index.js
+++ hooks-seasons/src/index.js
@@ -1,53 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
-class App extends React.Component {
+const App = () => {
+  const [ lat, setLat ] = useState(null);
+  const [ errorMessage, setErrorMessage ] = useState('');
 
-  // Initialize State
-  state = {
-    lat: null,
-    errorMessage: ''
-  };
-
-  renderContent() {
-    if (!this.state.lat && this.state.errorMessage)
-      return <div>Error: { this.state.errorMessage }</div>;
-
-    if (this.state.lat && !this.state.errorMessage)
-      return <SeasonDisplay lat={this.state.lat} />;
-
-    return <Spinner message="Please accept location request" />;  // Else - loading
-  }
-
-  render() {
-    return (
-      <div className="border red">
-        {this.renderContent()}
-      </div>
-    );
-  }
-
-  // Called the first time the component is rendered - best practice data-loading fn
-  componentDidMount() {
+  useEffect(() => {
     // Request location:
     window.navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({ lat:position.coords.latitude }),  // Success Callback
-      (error) => this.setState({ errorMessage:error.message })        // Error Callback
+      (position) => setLat(position.coords.latitude),  // Success Callback
+      (error) => setErrorMessage(error.message)        // Error Callback
     );
-  }
+  }, []);
 
-  // Called (after `render`) each time the component updates itself
-  componentDidUpdate() {
-    console.log('My component was just updated - it rerendered!');
-  }
+  let content;
+  if (!lat && errorMessage)
+    content = <div>Error: { this.state.errorMessage }</div>;
+  else if (lat)
+    content = <SeasonDisplay lat={lat} />;
+  else
+    content = <Spinner message="Please accept location request" />;
 
-  // Called after the component is removed
-  componentWillUnmount() {
-    console.log('My component has been cleaned up.')
-  }
-
-}
+  return <div className="border red">{content}</div>;
+};
 
 ReactDOM.render(<App />, document.getElementById('root'))
