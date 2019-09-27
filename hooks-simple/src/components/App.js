3bfae91f2713470eff63545a860f75ec4e1470b6
Gabriel Dibble <gdibble@gmail.com>
2019-09-26 22:57:38 -0700

    Section 25: Boilerplate hooks-simple app

---
+++ hooks-simple/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="ui container">
+        App
+      </div>
+    );
+  }
+
+};
+
+export default App;
